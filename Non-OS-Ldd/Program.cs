52bb46f2e4c97b6056ee6ebacdec4466de865d3f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-25 15:56:49 +0800

    RootCommand

--- Non-OS-Ldd/Program.cs
+++ Non-OS-Ldd/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+using JCNET.命令行;
+
+if (true)
+{
+	RootCommand root_cmd = [];
+
+	Option<bool> debug_option = new("--debug", "使用此参数表示开启调试模式。");
+	root_cmd.AddOptionAndHandler(debug_option, (bool debug) =>
+	{
+		if (debug)
+		{
+			Console.WriteLine("调试模式");
+		}
+		else
+		{
+			Console.WriteLine("发布模式。");
+		}
+	});
+
+	int cmd_parse_result = await root_cmd.InvokeAsync(args);
+	if (cmd_parse_result != 0)
+	{
+		return cmd_parse_result;
+	}
+}
+
+return 0;
