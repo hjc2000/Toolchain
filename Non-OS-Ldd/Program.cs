15fd2f9a303110aa0d4374495d4350ee428da5b2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-26 14:45:09 +0800

    SetHandler

--- Non-OS-Ldd/Program.cs
+++ Non-OS-Ldd/Program.cs
@@ -1,5 +1,7 @@
 using JCNET.命令行;
 using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 
 Arguments arguments = new();
 
@@ -7,11 +9,26 @@ if (true)
 {
 	RootCommand root_cmd = [];
 
-	Option<string?> debug_option = new("--exe_path", "可执行文件路径。");
-	root_cmd.AddOptionAndHandler(debug_option, (string? exe_path) =>
+	Option<string> exe_path_option = new("--exe_path", "可执行文件路径。")
 	{
-		ArgumentNullException.ThrowIfNull(exe_path);
-		arguments.ExePath = exe_path;
+		IsRequired = true,
+	};
+
+	root_cmd.AddOption(exe_path_option);
+
+	Option<bool> copy_dll_option = new("--copy_dll",
+		"分析完依赖后，是否将依赖的 dll 拷贝到可执行文件所在的目录。" +
+		"（不包括系统 dll 和已经在可执行文件目录中的 dll.）")
+	{
+		IsRequired = false,
+	};
+
+	root_cmd.AddOption(copy_dll_option);
+
+	root_cmd.SetHandler((InvocationContext context) =>
+	{
+		arguments.ExePath = context.ParseResult.GetValueForOption(exe_path_option)!;
+		arguments.CopyDll = context.ParseResult.GetValueForOption(copy_dll_option);
 	});
 
 	int cmd_parse_result = await root_cmd.InvokeAsync(args);
@@ -21,11 +38,31 @@ if (true)
 	}
 }
 
-Console.WriteLine(arguments.ExeFullPath);
-IEnumerable<string> ldd_results = await PowershellExtension.GetNonOSDependentDllFullPathAsync(arguments.ExeFullPath);
-foreach (string str in ldd_results)
+string exe_dir = Path.GetDirectoryName(arguments.ExeFullPath)!.Replace('\\', '/');
+Console.WriteLine(exe_dir);
+
+IEnumerable<string> ldd_results =
+	await PowershellExtension.GetNonOSDependentDllFullPathAsync(arguments.ExeFullPath);
+
+foreach (string dependent_dll_full_path in ldd_results)
 {
-	Console.WriteLine(str);
+	if (dependent_dll_full_path.StartsWith(exe_dir))
+	{
+		continue;
+	}
+
+	if (arguments.CopyDll)
+	{
+		string dll_name = Path.GetFileName(dependent_dll_full_path);
+		Console.WriteLine($"{dependent_dll_full_path} => {exe_dir}/{dll_name}");
+		await using FileStream src_fs = File.OpenRead(dependent_dll_full_path);
+		await using FileStream dst_fs = File.OpenWrite($"{exe_dir}/{dll_name}");
+		await src_fs.CopyToAsync(dst_fs);
+	}
+	else
+	{
+		Console.WriteLine($"{dependent_dll_full_path}");
+	}
 }
 
 return 0;
@@ -41,4 +78,6 @@ internal class Arguments
 			return Path.GetFullPath(ExePath);
 		}
 	}
+
+	public bool CopyDll { get; set; } = false;
 }
