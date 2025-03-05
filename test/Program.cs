8445f5439480918c0117aeb8a4b5a27633cacb01
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-05 23:30:32 +0800

    public static async Task<List<string>> Cygpath_GetWindowsPath_Async(IEnumerable<string> paths)

---
+++ test/Program.cs
@@ -0,0 +1,8 @@
+﻿using JCNET.命令行;
+
+List<string> results = await PowershellExtension.LddAsync("F:/cpp-lib-build-scripts/msys/.total-install/bin/sync-time.exe");
+results = await PowershellExtension.Cygpath_GetWindowsPath_Async(results);
+foreach (string result in results)
+{
+	Console.WriteLine(result);
+}
